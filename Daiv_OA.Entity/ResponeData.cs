47bd1b9d5e355a4e7fba7ce2915d3eb9b5093181
fanyongjian <fanyongjian@banggood.com>
2018-12-03 09:15:44 +0800

    新增返回值类型

---
+++ Daiv_OA.Entity/ResponeData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Daiv_OA.Entity
+{
+    public class ResponeData
+    {
+        /// <summary>
+        /// 数据状态
+        /// </summary>
+        public int Status { get; set; }
+        /// <summary>
+        /// 操作信息
+        /// </summary>
+        public string Msg { get; set; }
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        public object Data { get; set; }
+    }
+}
